7446ded63fc810a6dc06351532e3fe8d3c471363
dajooo <contact@dajooo.de>
2022-08-11 20:36:45 +0200

    fix: add @nuxtjs/web-vitals so it hopefully won't fail on ci build fix: use middleware for redirect

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-const router = useRouter()
-
-router.push({
-  name: 'agents',
+definePageMeta({
+  middleware: 'agents-redirect',
 })
 </script>
 
 <template>
-  <span />
+  <div class="flex flex-col justify-center items-center">
+    <p>You shouldn't be here!</p>
+    <NuxtLink class="text-blue-400" to="/agents">
+      You better click me right now!
+    </NuxtLink>
+  </div>
 </template>
