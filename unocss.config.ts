9a723508cb1905b9fe96a20301315c114d2c3039
dajooo <contact@dajooo.de>
2022-12-21 12:26:00 +0100

    chore: some minor optimizations

--- unocss.config.ts
+++ unocss.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     [/^text-shadow-(.+)$/, colorResolver('--un-text-shadow-color', 'text-shadow'), { autocomplete: 'text-shadow-$colors' }],
   ],
   shortcuts: [
-    ['btn', 'px-2 py-1 md:(px-8 py-2) rounded text-white cursor-pointer disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50 shadow-sm'],
+    ['btn', 'px-2 py-2 md:(px-8 py-2) rounded text-white cursor-pointer disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50 shadow-sm'],
     ['btn-primary', 'bg-blue-600 text-white hover:bg-blue-700'],
     ['btn-error', 'bg-red-600 text-white hover:bg-red-700'],
     ['btn-success', 'bg-green-700 text-white hover:bg-green-800'],
